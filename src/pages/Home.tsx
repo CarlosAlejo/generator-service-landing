ec15ec223444186387b74417b19b4971a2ba6dd1
Carlos <30472694+CarlosAlejo@users.noreply.github.com>
2026-05-13 00:21:06 -0500

    Imagen motor

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -26,7 +26,7 @@ export default function Home() {
         <div className="absolute inset-0 z-0">
           <img 
             src="https://pub-5ad9781a36b548ceb25cb8ae48abd358.r2.dev/motor1.avif?auto=format,compress&fit=crop&q=60&w=1280" 
-            srcSet="https://pub-5ad9781a36b548ceb25cb8ae48abd358.r2.dev/motor1.avif?auto=format,compress&fit=crop&q=60&w=640 640w, https://pub-5ad9781a36b548ceb25cb8ae48abd358.r2.dev/motor1.avif?auto=format,compress&fit=crop&q=60&w=1280 1280w, https://pub-5ad9781a36b548ceb25cb8ae48abd358.r2.dev/motor.avif?auto=format,compress&fit=crop&q=75&w=1920 1920w"
+            srcSet="https://pub-5ad9781a36b548ceb25cb8ae48abd358.r2.dev/motor1.avif?auto=format,compress&fit=crop&q=60&w=640 640w, https://pub-5ad9781a36b548ceb25cb8ae48abd358.r2.dev/motor1.avif?auto=format,compress&fit=crop&q=60&w=1280 1280w, https://pub-5ad9781a36b548ceb25cb8ae48abd358.r2.dev/motor1.avif?auto=format,compress&fit=crop&q=75&w=1920 1920w"
             sizes="100vw"
             className="w-full h-full object-cover brightness-[0.25]"
             alt="Ingeniería de precisión y mantenimiento industrial especializado de RMF Motor's"
